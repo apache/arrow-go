8923842efd043751e3ba7c841241f70568162d17
Victor Perez <vperez@timescale.com>
2025-05-09 10:52:54 +0200

    fix: use totalValues instead of nvals
    I'm unsure about what the difference is, but by looking at version v17 code, I noticed a difference between Int32 and Int64 decoder. Int32 used totalValues and Int64 used nvals.
    
    Looks like when merging implementations, the Int64 approach was used. Which seems to not be working properly for Int32.
    
    This change at least seems to be able to read the test file, which makes me think it is the right approach.

--- parquet/internal/encoding/delta_bit_packing.go
+++ parquet/internal/encoding/delta_bit_packing.go
@@ -197,7 +197,7 @@ func (d *deltaBitPackDecoder[T]) Discard(n int) (int, error) {
 // Decode retrieves min(remaining values, len(out)) values from the data and returns the number
 // of values actually decoded and any errors encountered.
 func (d *deltaBitPackDecoder[T]) Decode(out []T) (int, error) {
-	max := shared_utils.Min(len(out), int(d.nvals))
+	max := shared_utils.Min(len(out), int(d.totalValues))
 	if max == 0 {
 		return 0, nil
 	}
