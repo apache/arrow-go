8be5e697a21578d61d4b4ea75516099d4a34a451
Kristofer Gaudel <68076186+kris-gaudel@users.noreply.github.com>
2025-06-12 12:47:04 -0400

    feat(arrow/_examples): enhance library examples (#394)
    ### Rationale for this change
    Enhancing the library examples
    
    ### What changes are included in this PR?
    3 new examples on how to use the `arrow-go` library:
    - Reading from a CSV file
    - Type conversions
    - Managing performance
    
    ### Are these changes tested?
    Not applicable
    
    ### Are there any user-facing changes?
    No, just enhancing examples

--- ci/scripts/tinygo_example.sh
+++ ci/scripts/tinygo_example.sh
@@ -21,7 +21,7 @@ set -eux
 
 cd ~
 pushd /src
-tinygo build -tags noasm -o ~/example_tinygo arrow/_examples/helloworld/main.go
+tinygo build -tags noasm -o ~/example_tinygo arrow/examples/table_creation/main.go
 popd
 
 ./example_tinygo
