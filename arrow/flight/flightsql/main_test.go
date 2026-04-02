5cd609008e53d342f03c1a22bbdaea1a90b3dd80
Matt Topol <zotthewizard@gmail.com>
2026-04-02 13:56:14 -0400

    ci: skip flaky flight/flightsql tests in CI with -short flag
    Add TestMain to arrow/flight and arrow/flight/flightsql that skips
    all tests when -short is set. Pass -short in ci/scripts/test.sh for
    the arrow test invocations only (parquet tests unaffected).
    
    These packages have gRPC server/client tests with inherent timing
    races that spuriously fail under -race/-asan in CI. Running locally
    without -short still exercises them.

---
+++ arrow/flight/flightsql/main_test.go
@@ -0,0 +1,35 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package flightsql_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// FlightSQL tests involve gRPC server/client interactions that are
+	// inherently racy under -race/-asan and spuriously fail in CI.
+	// Use -short in CI to skip them; they still run locally via
+	// `go test ./...` (without -short).
+	if testing.Short() {
+		fmt.Println("SKIP: flightsql tests disabled with -short flag")
+		os.Exit(0)
+	}
+	os.Exit(m.Run())
+}
