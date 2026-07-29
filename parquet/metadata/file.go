feb71eb53731f444549c9ab0fcbc499bb8e7df03
Minh Vu <vuhoangminh97@gmail.com>
2026-07-29 18:53:28 +0200

    fix(parquet/metadata): propagate schema initialization errors

--- parquet/metadata/file.go
+++ parquet/metadata/file.go
@@ -317,7 +317,9 @@ func NewFileMetaData(data []byte, fileDecryptor encryption.FileDecryptor) (*File
 		FileDecryptor: fileDecryptor,
 	}
 
-	f.initSchema()
+	if err := f.initSchema(); err != nil {
+		return nil, err
+	}
 	f.initColumnOrders()
 
 	return f, nil
