047415445062eb94546075467be9f377a255cfaf
Minh Vu <vuhoangminh97@gmail.com>
2026-07-29 19:14:07 +0200

    fix(parquet/pqarrow): clean up partial extension chunks

--- parquet/pqarrow/file_reader.go
+++ parquet/pqarrow/file_reader.go
@@ -135,14 +135,16 @@ func (er *extensionReader) BuildArray(boundedLen int64) (*arrow.Chunked, error) 
 	extType := er.fieldWithExt.Type.(arrow.ExtensionType)
 
 	newChunks := make([]arrow.Array, len(chkd.Chunks()))
+	defer func() {
+		for _, c := range newChunks {
+			if c != nil {
+				c.Release()
+			}
+		}
+	}()
 	for i, c := range chkd.Chunks() {
 		newChunks[i] = array.NewExtensionArrayWithStorage(extType, c)
 	}
-	defer func() {
-		for _, c := range newChunks {
-			c.Release()
-		}
-	}()
 
 	return arrow.NewChunked(extType, newChunks), nil
 }
