67d9a5d8fb50b64fcf185ae51052ff3b0ae17b45
Minh Vu <vuhoangminh97@gmail.com>
2026-08-07 12:18:38 +0200

    fix(memory): handle zero-length checked reallocations

--- arrow/memory/checked_allocator.go
+++ arrow/memory/checked_allocator.go
@@ -68,14 +68,22 @@ func (a *CheckedAllocator) Allocate(size int) []byte {
 func (a *CheckedAllocator) Reallocate(size int, b []byte) []byte {
 	a.sz.Add(int64(size - len(b)))
 
-	oldptr := uintptr(unsafe.Pointer(&b[0]))
+	var oldptr uintptr
+	if cap(b) > 0 {
+		oldptr = uintptr(unsafe.Pointer(&b[:1][0]))
+	}
 	out := a.mem.Reallocate(size, b)
 	if size == 0 {
+		if oldptr != 0 {
+			a.allocs.Delete(oldptr)
+		}
 		return out
 	}
 
 	newptr := uintptr(unsafe.Pointer(&out[0]))
-	a.allocs.Delete(oldptr)
+	if oldptr != 0 {
+		a.allocs.Delete(oldptr)
+	}
 	pcs := make([]uintptr, maxRetainedFrames)
 
 	// For historical reasons the meaning of the skip argument
