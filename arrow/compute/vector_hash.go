4432491a748aac3f88f70675d576bae9143e2428
Minh Vu <vuhoangminh97@gmail.com>
2026-08-08 17:03:23 +0200

    docs(compute): clarify unique null behavior

--- arrow/compute/vector_hash.go
+++ arrow/compute/vector_hash.go
@@ -30,7 +30,7 @@ import (
 var (
 	uniqueDoc = FunctionDoc{
 		Summary:     "Compute unique elements",
-		Description: "Return an array with distinct values. Nulls in the input are ignored",
+		Description: "Return an array with distinct values. Nulls in the input are considered a distinct value",
 		ArgNames:    []string{"array"},
 	}
 	dictionaryEncodeDoc = FunctionDoc{
