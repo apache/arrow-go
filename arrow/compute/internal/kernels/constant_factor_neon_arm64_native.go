a87cf19d22442843964091cd518ca570a4eaa441
Minh Vu <vuhoangminh97@gmail.com>
2026-08-30 22:44:04 +0200

    fix(compute): restrict NEON declarations to ARM64

--- arrow/compute/internal/kernels/constant_factor_neon_arm64_native.go
+++ arrow/compute/internal/kernels/constant_factor_neon_arm64_native.go
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.18 && !noasm && !appengine
+//go:build go1.18 && arm64 && !noasm && !appengine
 
 package kernels
 
